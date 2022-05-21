685a35bba2433a2c3b5e4b6004da9b48d80ebb64
Palma Kopic <palma.kopic@tacta.io>
2022-05-21 07:57:47 +0200

    Azuriraj dogadaj test

--- festival-backend/src/test/java/hr/fer/infosus/festivalbackend/integration/DogadajIntegrationTest.java
+++ festival-backend/src/test/java/hr/fer/infosus/festivalbackend/integration/DogadajIntegrationTest.java
@@ -6,7 +6,6 @@ import hr.fer.infosus.festivalbackend.domain.Mjesto;
 import hr.fer.infosus.festivalbackend.repository.DogadajRepository;
 import hr.fer.infosus.festivalbackend.repository.MjestoRepository;
 import hr.fer.infosus.festivalbackend.web.rest.dto.DogadajDto;
-import hr.fer.infosus.festivalbackend.web.rest.dto.MjestoDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -14,8 +13,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 public class DogadajIntegrationTest extends BaseIntegrationTest {
@@ -75,6 +73,37 @@ public class DogadajIntegrationTest extends BaseIntegrationTest {
         Dogadaj dogadajRes = dogadajRepository.getById(dogadaj.getId());
 
         assertEquals(dogadajRes, null);
+    }
+
+    @Test
+    void updateDogadaj() throws Exception {
+        Mjesto mjesto = mjestoRepository.save(Mjesto.builder().naziv("Zagreb").postanskiBroj(10000).build());
+        Dogadaj dogadaj = dogadajRepository.save(Dogadaj.builder()
+                .brojUlaznica(5261)
+                .datumPocetka(LocalDate.of(2022, 12, 2))
+                .naziv("Lijep Festival")
+                .trajanjeDani(2)
+                .mjesto(mjesto)
+                .build());
+
+        DogadajDto dogadajDto = DogadajDto.builder()
+                .id(dogadaj.getId())
+                .brojUlaznica(5261)
+                .datumPocetka(LocalDate.of(2022, 12, 2))
+                .naziv("Lijep FestivalUpdated")
+                .trajanjeDani(2)
+                .mjesto(mjesto)
+                .build();
+
+        mockMvc.perform(put("/api/v1/dogadaj/")
+                        .contentType(APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(dogadajDto)))
+                .andExpect(status().is2xxSuccessful());
+
+        Dogadaj dogadajRes = dogadajRepository.getById(dogadaj.getId());
+
+        assertEquals("Lijep FestivalUpdated", dogadajRes.getNaziv());
+
     }
 
 }
